4a130005850fedd54cbfdb1349f9734c075e74cb
shiva kumar <shiva9542kumar@gmail.com>
2024-12-29 17:47:14 +0000

    initial commit

--- src/declarations/token/token.did.js
+++ src/declarations/token/token.did.js
@@ -1,6 +1,6 @@
 export const idlFactory = ({ IDL }) => {
   return IDL.Service({
-    'balanceOf' : IDL.Func([IDL.Principal], [IDL.Nat], ['query']),
+    'balancesOf' : IDL.Func([IDL.Principal], [IDL.Nat], ['query']),
     'getSymbol' : IDL.Func([], [IDL.Text], ['query']),
     'payOut' : IDL.Func([], [IDL.Text], []),
     'transfer' : IDL.Func([IDL.Principal, IDL.Nat], [IDL.Text], []),
--- src/opend_assets/src/components/Item.jsx
+++ src/opend_assets/src/components/Item.jsx
@@ -1,21 +1,151 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
+import { Actor, HttpAgent } from "@dfinity/agent";
+import { idlFactory } from "../../../declarations/nft";
+import { idlFactory as t} from "../../../declarations/token";
+import { Principal } from "@dfinity/principal";
+import { opend } from "../../../declarations/opend";
+import Button from "./Button";
+import CURRENT_USER_ID from "../index";
+import PriceLabel from "./PriceLabel";
 
-function Item() {
+function Item(props) {
+  const [name, setName] = useState();
+  const [owner, setOwner] = useState();
+  const [image, setImage] = useState();
+  const [button, setButton] = useState();
+  const [priceInput, setPriceInput] = useState();
+  const [loaderHidden, setLoaderHidden] = useState(true);
+  const [blur,setblur] = useState();
+  const [sellStatus,setSellStatus] = useState();
+  const [priceLabel,setPriceLabel] = useState();
+  const [shouldDisplay, setShouldDisplay] = useState(true);
+
+  const id = props.id;
+
+  const localHost = "http://localhost:8080/";
+  const agent = new HttpAgent({ host: localHost });
+
+  //TODO: When deploy live, remove the following line.
+  agent.fetchRootKey();
+  let NFTActor;
+
+  async function loadNFT() {
+    NFTActor = await Actor.createActor(idlFactory, {
+      agent,
+      canisterId: id,
+    });
+
+    const name = await NFTActor.getName();
+    const owner = await NFTActor.getOwner();
+    const imageData = await NFTActor.getAsset();
+    const imageContent = new Uint8Array(imageData);
+    const image = URL.createObjectURL(
+      new Blob([imageContent.buffer], { type: "image/png" })
+    );
+
+    setName(name);
+    setOwner(owner.toText());
+    setImage(image);
+    const nftIsListed = await opend.isListed(props.id);
+    if(props.role == "collection")
+    {
+      if(nftIsListed){
+        setOwner("openD");
+        setblur({filter: "blur(4px)"});
+        setSellStatus("Listed");
+      }else{
+      setButton(<Button handleClick={handleSell} text={"Sell"} />);
+      }
+    }
+    else if(props.role == "discover"){
+      const originalOwner = await opend.getOriginalOwner(props.id);
+      if(originalOwner.toText() != CURRENT_USER_ID.toText()){
+        setButton(<Button handleClick={handleBuy} text={"Buy"} />);
+      }
+      const price = await opend.getListedNFTPrice(props.id);
+      setPriceLabel(<PriceLabel sellPrice = {price.toString()}/>);
+    }
+  }
+  useEffect(() => {
+    loadNFT();
+  }, []);
+
+  let price;
+  function handleSell() {
+    console.log("Sell clicked");
+    setPriceInput(
+      <input
+        placeholder="Price in NN"
+        type="number"
+        className="price-input"
+        value={price}
+        onChange={(e) => (price = e.target.value)}
+      />
+    );
+    setButton(<Button handleClick={sellItem} text={"Confirm"} />);
+  }
+
+  async function sellItem() {
+    setLoaderHidden(false);
+    console.log("set price = " + price);
+    const listingResult = await opend.listItem(props.id, Number(price));
+    console.log("listing: " + listingResult);
+    if (listingResult == "Success") {
+      const openDId = await opend.getOpenDCanisterID();
+      const transferResult = await NFTActor.transferOwnership(openDId, true);
+      console.log("transfer: " + transferResult);
+      if(transferResult == "Success"){
+        setLoaderHidden(true);
+        setButton();
+        setPriceInput();
+        setblur({filter: "blur(4px)"});
+        setOwner("openD");
+      }
+    } 
+  }
+  async function handleBuy(){
+    setLoaderHidden(false);
+    console.log("buy is clicked");
+    const tokenActor = await Actor.createActor(t,{
+      agent,
+      canisterId: Principal.fromText("tlwi3-3aaaa-aaaaa-aaapq-cai"),
+    });
+    const sellerId = await opend.getOriginalOwner(props.id);
+    const itemPrice = await opend.getListedNFTPrice(props.id);
+    const result = await tokenActor.transfer(sellerId,itemPrice);
+    if(result == "success"){
+      const transferResult = await opend.completePurchase(id,sellerId,CURRENT_USER_ID);
+      console.log("purchase: "+ transferResult);
+      setLoaderHidden(true);
+      setShouldDisplay(false);
+    }
+  }
   return (
-    <div className="disGrid-item">
+    <div style = {{display: shouldDisplay? "inline": "none"}} className="disGrid-item">
       <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
         <img
           className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
-          src={logo}
+          src={image}
+          style={blur}
         />
+        <div hidden={loaderHidden} className="lds-ellipsis">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
         <div className="disCardContent-root">
+         {priceLabel}
           <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
-            CryptoDunks #312<span className="purple-text"></span>
+            {name}
+            <span className="purple-text"> {sellStatus}</span>
           </h2>
           <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
-            Owner: sdfsdf-erwerv-sdf
+            Owner: {owner}
           </p>
+          {priceInput}
+          {button}
         </div>
       </div>
     </div>
